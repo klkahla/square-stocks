48d6485682bd5e2600796c1b944e34fe4482177f
Katy Kahla <katykahla@gmail.com>
2023-06-05 20:59:49 -0600

    Initial app commit
    Basic app

---
+++ cash-app-stocks/cash-app-stocks/cash_app_stocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  cash_app_stocksApp.swift
+//  cash-app-stocks
+//
+//  Created by Katy Kahla on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct cash_app_stocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
